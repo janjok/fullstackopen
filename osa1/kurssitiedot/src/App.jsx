e297a4252002f63674c81fc8ec8e207d535bee06
janjok <118051663+janjok@users.noreply.github.com>
2024-07-20 18:49:02 +0300

    Done to 2.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,47 +1,4 @@
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name}/>
-      <Content parts={props.course.parts}/>
-      <Total total={props.course.parts}/>
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h2>{props.course}</h2>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.parts.name} {props.parts.exercises}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  const { parts } = props
-  return (
-    <div>
-      {parts.map(part => 
-        <Part key={part.id} parts={part} />
-      )}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  let { total } = props
-  let amount = total.reduce((a, c) => a + c.exercises, 0)
-  return (
-    <div>
-        <p><b>total of {amount} exercises</b></p>
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
