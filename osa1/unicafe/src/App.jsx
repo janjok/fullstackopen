f5e0aef66592220c2c78da554072704edbc46b9f
janjok <118051663+janjok@users.noreply.github.com>
2026-01-11 15:48:54 +0200

    o1 t1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -2,7 +2,19 @@ import { useState } from 'react';
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Stats = ({ text, total }) => <p>{text} {total}</p>
+const Stats = ({ text, amount }) => <p>{text} {amount}</p>
+
+const Total = ({ total }) => <p>all {total}</p>
+
+const Average = ({ g, n, b}) => {
+  const allVotes = g + n + b
+
+  return (
+    <p>average {(g-b) / allVotes}</p>
+  )
+}
+
+const Positive = ({ t, g }) => <p>positive {g / t} %</p>
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -15,12 +27,13 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text='good' />
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button onClick={() => setBad(bad + 1)} text='bad' />
-      <div>
-        <h2>statistics</h2>
-        <Stats text='good' total={good} />
-        <Stats text='neutral' total={neutral} />
-        <Stats text='bad' total={bad} />
-      </div>
+      <h2>statistics</h2>
+      <Stats text='good' amount={good} />
+      <Stats text='neutral' amount={neutral} />
+      <Stats text='bad' amount={bad} />
+      <Total total={good + neutral + bad} />
+      <Average g={good} n={neutral} b={bad} />
+      <Positive t={good + neutral + bad} g={good} />
     </div>
   )
 }
